c04b9a89faf8c0c98f75a415dccd2cdc59cfd6cb
sitoto <huang.gadela@gmail.com>
2014-10-07 21:44:32 +0800

    download

--- tianyapic.rb
+++ tianyapic.rb
@@ -1,3 +1,4 @@
+require 'mechanize'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -19,6 +20,19 @@ class GetSomeThing
   def read_list
     puts "ok?"
   end
+
+  def download_pic
+#    url = "http://img3.laibafile.cn/p/m/180695272.jpg"
+    url = "http://img3.laibafile.cn/p/m/180688971.jpg"
+
+    puts url
+    agent = Mechanize.new
+    page = agent.get(url, [], referer = "http://bbs.tianya.cn/").save
+    page = agent.get(url).save
+
+
+  end
+
 end
 
-GetSomeThing.new().read_list
+GetSomeThing.new().download_pic
